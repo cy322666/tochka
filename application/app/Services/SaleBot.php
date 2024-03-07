90d775018a343c4c0838e074de7cc098ab7be327
cy322666 <cy322666@gmail.com>
2024-03-07 12:46:17 +0200

    sb fix

--- application/app/Services/SaleBot.php
+++ application/app/Services/SaleBot.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Http;
 
 class SaleBot
 {
-    public static string $token;
-
     private static string $baseUrl = 'https://chatter.salebot.pro/api/';
+
+    public function __construct(public string $token) {}
 
     public function unsubscribe(int $listId, int $clientId): ?object
     {
-        return Http::post(static::$baseUrl.static::$token.'/remove_from_list', [
+        return Http::post(static::$baseUrl.$this->token.'/remove_from_list', [
             'list_id' => $listId,
             'clients' => $clientId,
         ])->object();
