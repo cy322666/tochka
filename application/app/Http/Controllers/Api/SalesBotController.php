3f3a9fad225655c466c68a2441c16d0c74044cb9
cy322666 <cy322666@gmail.com>
2024-03-07 11:51:51 +0200

    sb fix

--- application/app/Http/Controllers/Api/SalesBotController.php
+++ application/app/Http/Controllers/Api/SalesBotController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Jobs\Api\SalesBot\GetHook;
-use App\Models\Api\SalesBot\FilterContecst;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 
 class SalesBotController extends Controller
 {
