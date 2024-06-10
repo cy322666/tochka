6277fdc05d8208cf9ae53d52383da4a32b15bcc4
cy322666 <cy322666@gmail.com>
2024-06-10 11:18:57 +0200

    add comm fail

---
+++ application/app/Console/Commands/Api/Platform/SendFail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands\Api\Platform;
+
+use App\Models\Platform\Order;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SendFail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-fail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $orders = Order::query()->where('status', false)->get();
+
+        foreach ($orders as $order) {
+
+            Artisan::call('platform:send-order', ['order_id' => $order->id]);
+        }
+    }
+}
