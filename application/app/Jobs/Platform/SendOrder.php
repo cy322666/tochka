41132fd4f179d634404dea352301deef3d1b582f
cy322666 <cy322666@gmail.com>
2024-08-20 09:19:08 +0200

    add order part

--- application/app/Jobs/Platform/SendOrder.php
+++ application/app/Jobs/Platform/SendOrder.php
@@ -145,6 +145,17 @@ class SendOrder implements ShouldQueue
 
         $lead = $amoApi->service->leads()->find($lead->id);
 
+        //тут частичная оплата или нет
+        if ($this->order->status_order == 'Частично оплачен' &&
+            ($this->order->cost_money == ($this->order->left_cost_money + $this->order->payed_money))) {
+
+                $lead->cf('Первый платёж по заказу')->enable();
+
+        } else
+            $lead->cf('Первый платёж по заказу')->disable();
+
+        $lead->save();
+
         $this->order->lead_id = $lead->id;
         $this->order->status_id = $lead->status_id;
         $this->order->pipeline_id = $lead->pipeline_id;
