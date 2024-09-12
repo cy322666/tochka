9f26e585131ec59cf96dff0da82ed951308045a3
cy322666 <cy322666@gmail.com>
2024-09-12 13:11:42 +0300

    ord hotfix

--- application/app/Console/Commands/Api/Ord/CreateContract.php
+++ application/app/Console/Commands/Api/Ord/CreateContract.php
@@ -51,7 +51,7 @@ class CreateContract extends Command
         $lead = $amoApi->service->leads()->find($transaction->lead_id);
 
         $searchBaseContract = Contract::query()
-            ->where('serial', $lead->company->cf('Номер договора')->getValue())
+            ->where('contractor_external_id', $transaction->person_uuid)
             ->where('type', 'service')
             ->first();
 
