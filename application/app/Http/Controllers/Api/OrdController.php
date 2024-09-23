6cb5a05a65aaffd1a8f38fe6f48a16f0c12b86a3
cy322666 <cy322666@gmail.com>
2024-09-23 11:20:55 +0200

    ord fix

--- application/app/Http/Controllers/Api/OrdController.php
+++ application/app/Http/Controllers/Api/OrdController.php
@@ -7,6 +7,7 @@ use App\Models\Api\Ord\Person;
 use App\Models\Api\Ord\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class OrdController extends Controller
 {
@@ -18,12 +19,22 @@ class OrdController extends Controller
 
         $result1 = Artisan::call('ord:create-person',   ['transaction' => $transaction->id]);
 
+        Log::debug('ord:create-person : '.$transaction->id.' > '.$result1);
+
         if ($result1)
             $result2 = Artisan::call('ord:create-contract', ['transaction' => $transaction->id]);
+
+        Log::debug('ord:create-contract : '.$transaction->id.' > '.$result2);
+
         if ($result2)
             $result3 = Artisan::call('ord:create-pad',      ['transaction' => $transaction->id]);
+
+        Log::debug('ord:create-pad : '.$transaction->id.' > '.$result3);
+
         if ($result3)
             $result4 = Artisan::call('ord:create-creative', ['transaction' => $transaction->id]);
+
+        Log::debug('ord:create-creative : '.$transaction->id.' > '.$result4);
     }
 
     public function invoice(Request $request)
